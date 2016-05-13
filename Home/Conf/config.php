86a040c7480050b5de3b176f30fc44807baf4a85
engine <engine@engine.local>
2016-05-13 13:55:50 +0800

    init

---
+++ Home/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DB_TYPE'                => 'mysql', // 数据库类型
+    'DB_HOST'                => '120.25.203.16', // 服务器地址
+    'DB_NAME'                => 'app_sdk', // 数据库名
+    'DB_USER'                => 'engine', // 用户名
+    'DB_PWD'                 => 'kdal254379', // 密码
+    'DB_PORT'                => '3306', // 端口
+    'DB_PREFIX'              => '', // 数据库表前缀
+);
